778f5953bf75a3e7330711b76f2afd0a1b1a3f1f
CucchiettiNicola <46776635+CucchiettiNicola@users.noreply.github.com>
2019-01-22 10:19:48 +0100

    Add files via upload

---
+++ progettoAlternanza/Assets/Nemici/intelligence.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class intelligence : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float walk = -200f;
+
+    void FixedUpdate() {
+        rb.AddForce(0, 0, walk * Time.deltaTime);
+    }
+}
